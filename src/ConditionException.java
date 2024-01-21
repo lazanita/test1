614831a87709e57e9be01c6222dd016f979d635e
lazanita <anna.anla.lazareva@mail.ru>
2024-01-22 02:21:51 +0300

    Initial commit

---
+++ src/ConditionException.java
@@ -0,0 +1,6 @@
+public class ConditionException extends Exception{
+    public ConditionException(){
+        System.out.println("throws Exception");
+        System.exit(-1);
+    }
+}
---
+++ src/Main.java
@@ -0,0 +1,49 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) throws ConditionException {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Введите целочисленный пример: ");
+        System.out.println(calc(sc.nextLine()));
+            }
+    public static String calc(String input) throws ConditionException {
+        int count = 0, n=0;
+        char c = '0';
+        for ( char equSimb: input.toCharArray()) {
+            if (equSimb == '+' || equSimb == '-' || equSimb == '*' || equSimb == '/') {
+                c = equSimb;
+                count++;
+               n=input.indexOf(equSimb);
+            }
+        }
+        int a=0,b=0;
+        try {
+            a = Integer.parseInt(input.substring(0, n));
+            b = Integer.parseInt(input.substring((n+1), input.length()));
+        } catch (NumberFormatException e) {
+            throw new ConditionException();
+        }
+        if ((count!=1)||((a <= 0 || a > 11)||(b <= 0 || b> 11))) {
+            throw new ConditionException();
+        }
+        int result = 0;
+        switch (c) {
+            case '+':
+                result = a + b;
+                break;
+            case '-':
+                result = a - b;
+                break;
+            case '/':
+                result = a/b;
+                break;
+            case '*':
+                result = a*b;
+                break;
+        }
+        return Integer.toString(result);
+    }
+        }
+
+
+
